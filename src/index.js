4cc0533ec468753681eca82991b210cff8c3265a
Nicola <nicola.tamburini@fjstudio.com>
2021-09-30 12:21:25 +0200

    commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+require("./models/User");
+const express  = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require("./routes/authRoutes");
+const requireAuth = require("./middlewares/requireAuth");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri = 'mongodb+srv://admin:admin@cluster0.85e5c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+mongoose.connect(mongoUri);
+
+mongoose.connection.on('connected', () => { console.log('connected to mongo') });
+mongoose.connection.on('error', (err) => { console.error('error ', err)});
+
+app.get('/', requireAuth, (req,res) => {
+    res.send(`Your Email is ${req.user.email}`);
+});
+
+
+
+app.listen(3000, () => {
+    console.log('Listen in port 3000');
+});
